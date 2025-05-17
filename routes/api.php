b849c0ce0af4c33e5d5218b857d8ba667b3c04fe
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-16 23:31:33 -0300

    :sparkles: feat(public): endpoint registro de usuário

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+});
