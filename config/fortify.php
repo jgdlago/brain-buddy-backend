8fa1367307b19ce6616ab4379bd21f81bb550f55
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-16 23:15:06 -0300

    :sparkles: feat(public): adiciona configurações para API

--- config/fortify.php
+++ config/fortify.php
@@ -130,7 +130,7 @@ return [
     |
     */
 
-    'views' => true,
+    'views' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -146,14 +146,14 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
+         Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
+//        Features::twoFactorAuthentication([
+//            'confirm' => true,
+//            'confirmPassword' => true,
             // 'window' => 0,
-        ]),
+//        ]),
     ],
 
 ];
