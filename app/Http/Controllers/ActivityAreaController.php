386670e25723ca7e6ed9ac4519006d2fd2f97d0c
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-20 22:44:26 -0300

    :sparkles: feat(public): adiciona macro para listagem de collections

---
+++ app/Http/Controllers/ActivityAreaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ActivityAreaRequest;
+use App\Http\Resources\ActivityAreaResource;
+use App\Models\ActivityArea;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class ActivityAreaController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    public function index(Request $request): LengthAwarePaginator
+    {
+        return ActivityArea::getToApi($request);
+    }
+
+    /**
+     * @param ActivityAreaRequest $request
+     * @return JsonResource
+     */
+    public function store(ActivityAreaRequest $request): JsonResource
+    {
+        return new ActivityAreaResource(
+            ActivityArea::create($request->validated())
+        );
+    }
+
+    /**
+     * @param ActivityArea $activityArea
+     * @return JsonResource
+     */
+    public function show(ActivityArea $activityArea): JsonResource
+    {
+        return new ActivityAreaResource($activityArea);
+    }
+
+    /**
+     * @param ActivityArea $activityArea
+     * @param ActivityAreaRequest $request
+     * @return JsonResource
+     */
+    public function update(ActivityArea $activityArea, ActivityAreaRequest $request): JsonResource
+    {
+        $activityArea->update($request->validated());
+        return new ActivityAreaResource($activityArea);
+    }
+
+    /**
+     * @param ActivityArea $activityArea
+     * @return Response
+     */
+    public function destroy(ActivityArea $activityArea): Response
+    {
+        $activityArea->delete();
+        return response()->noContent(204);
+    }
+
+    /**
+     * @return Response
+     */
+    public function list(): Response
+    {
+        return response(
+            ActivityArea::all()->list('id', 'name')
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ActivityAreaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\{
@@ -16,4 +17,10 @@ Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisteredUserController::class, 'store']);
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
     Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
+});
+
+Route::apiResource('activity-area', ActivityAreaController::class);
+
+Route::prefix('list')->group(function () {
+    Route::get('activity-area', [ActivityAreaController::class, 'list']);
 });
