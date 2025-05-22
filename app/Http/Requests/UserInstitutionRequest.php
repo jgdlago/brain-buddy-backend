974df4ca7fb6e234cedfb8ef4be31bd9db832cb0
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-21 22:20:56 -0300

    :broom: cleanup(public): Request para vinculo usuário instituição

--- app/Http/Requests/UserInstitutionRequest.php
+++ app/Http/Requests/UserInstitutionRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class UserInstitutionRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,8 @@ class UserInstitutionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'institutions' => 'required|array',
+            'institutions.*.id' => 'required|integer|exists:App\Models\Institution,id',
         ];
     }
 }
