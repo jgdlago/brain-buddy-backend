a733c6172060ded506e2c1736af241a9aaef1f35
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-21 21:58:52 -0300

    :sparkles: feat(public): adiciona request e resource para Groups

--- app/Http/Resources/SimplyUserResource.php
+++ app/Http/Resources/SimplyUserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class GroupResource extends JsonResource
+class SimplyUserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,13 +14,9 @@ class GroupResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $this->load('responsibleUser', 'institution');
-
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'responsible_user' => new SimplyUserResource($this->responsibleUser),
-            'institution' => new SimplyInstitutionResource($this->institution)
+            'name' => $this->name
         ];
     }
 }
