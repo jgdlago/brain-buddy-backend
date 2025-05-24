22de02862b28de82eb0bd119315ab03a94473b40
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-05-24 10:50:34 -0300

    :sparkles: feat(public): adiciona precognition

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Http\Response;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Laravel\Fortify\Http\Controllers\{
     RegisteredUserController,
     AuthenticatedSessionController,
@@ -19,25 +19,27 @@ Route::get('/health-check', function () : Response
     return response('brain-buddy-backend is on', 200);
 });
 
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [RegisteredUserController::class, 'store']);
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
-});
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('activity-area', ActivityAreaController::class);
-    Route::apiResource('institution', InstitutionController::class);
-    Route::apiResource('group', GroupController::class);
-    Route::apiResource('player', PlayerController::class);
-
-    Route::prefix('list')->group(function () {
-        Route::get('activity-area', [ActivityAreaController::class, 'list']);
-        Route::get('institution', [InstitutionController::class, 'list']);
-        Route::get('group', [GroupController::class, 'list']);
-        Route::get('player', [PlayerController::class, 'list']);
+Route::middleware(HandlePrecognitiveRequests::class)->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [RegisteredUserController::class, 'store']);
+        Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
     });
 
-    Route::post('user/{user}/institutions', [UserController::class, 'addToInstitution']);
-    Route::delete('user/{user}/institution/{institution}', [UserController::class, 'addToInstitution']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::apiResource('activity-area', ActivityAreaController::class);
+        Route::apiResource('institution', InstitutionController::class);
+        Route::apiResource('group', GroupController::class);
+        Route::apiResource('player', PlayerController::class);
+
+        Route::prefix('list')->group(function () {
+            Route::get('activity-area', [ActivityAreaController::class, 'list']);
+            Route::get('institution', [InstitutionController::class, 'list']);
+            Route::get('group', [GroupController::class, 'list']);
+            Route::get('player', [PlayerController::class, 'list']);
+        });
+
+        Route::post('user/{user}/institutions', [UserController::class, 'addToInstitution']);
+        Route::delete('user/{user}/institution/{institution}', [UserController::class, 'addToInstitution']);
+    });
 });
