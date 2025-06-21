f7d40cd6fb7f2b23f1c613c30282134216fa271a
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-06-21 03:42:39 -0300

    :bug: fix(public): adiciona coluna para código da turma

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Group extends Model
 {
@@ -14,6 +15,7 @@ class Group extends Model
         'responsible_user_id',
         'institution_id',
         'education_level',
+        'code',
     ];
 
     /**
@@ -30,5 +32,43 @@ class Group extends Model
     public function institution(): BelongsTo
     {
         return $this->belongsTo(Institution::class);
+    }
+
+    /**
+     * @return void
+     */
+    protected static function booted(): void
+    {
+        static::creating(function ($group) {
+            $group->code = self::generateUniqueCode();
+        });
+    }
+
+    /**
+     * @return string
+     */
+    public static function generateUniqueCode(): string
+    {
+        $maxAttempts = 10;
+        $code = '';
+
+        do {
+            $randomPart = Str::upper(Str::random(4));
+
+            $numericPart = str_pad(mt_rand(0, 9999), 4, '0', STR_PAD_LEFT);
+
+            $code = $randomPart . $numericPart;
+        } while (self::codeExists($code) && $maxAttempts-- > 0);
+
+        return $code;
+    }
+
+    /**
+     * @param string $code
+     * @return bool
+     */
+    private static function codeExists(string $code): bool
+    {
+        return static::where('code', $code)->exists();
     }
 }
