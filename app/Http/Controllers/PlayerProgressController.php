13225dd87357a469d0ee92d525b1f41aabe45ba3
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-07-07 23:07:26 -0300

    :sparkles: feat:(public): documentação de parametros relatório de desempenho

--- app/Http/Controllers/PlayerProgressController.php
+++ app/Http/Controllers/PlayerProgressController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CharacterEnum;
+use App\Enums\EducationLevelEnum;
+use App\Enums\GenderEnum;
 use App\Services\PlayerProgressServiceReport;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
@@ -19,6 +23,14 @@ class PlayerProgressController extends Controller
      * @param Request $request
      * @return Collection
      */
+    #[QueryParameter('group', type: 'array')]
+    #[QueryParameter('player', type: 'array')]
+    #[QueryParameter('gender', type: GenderEnum::class)]
+    #[QueryParameter('education_level', type: EducationLevelEnum::class)]
+    #[QueryParameter('activity_area', type: 'array')]
+    #[QueryParameter('character', type: CharacterEnum::class)]
+    #[QueryParameter('age_min', type: 'int')]
+    #[QueryParameter('age_max', type: 'int')]
     public function index(Request $request): Collection
     {
         return collect($this->playerProgressServiceReport->getGraphData($request));
